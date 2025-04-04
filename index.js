44cea5e3020448b99481cae427fdc2c627a0b29c
shuQrah <hadiegy5@gmail.com>
2025-04-04 14:06:56 +0600

    initial commit

--- index.js
+++ index.js
@@ -1,36 +1,43 @@
 const express = require("express");
 const mongoose = require("mongoose");
-const nodemon = require("nodemon");
-const User = require("./models/User.mode");
+const User = require("./models/User.model");
 
 const app = express();
 
+app.use(express.json());
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
-app.get("/users", async (req, res) => {
-  const users = await user.find();
-  res.json(users);
-});
-
-app.get("/api/Users", async (req, res) => {
-  User.findByName()
-    .then((users) => {
-      res.send(users);
+app.post("/api/Users", async (req, res) => {
+  const user = new User({
+    name: req.body.name,
+    email: req.body.email,
+  });
+  user
+    .save(user)
+    .then((data) => {
+      res.send(data);
     })
     .catch((err) => {
       res.status(500).send({
-        message: err.message || "Some error occurred while retrieving users.",
+        message: err.message || "Some error occurred while creating the User.",
       });
     });
-  const user = await User.findByName(req.params.name);
-  res.send(user);
+});
+
+app.get("/api/Users", async (req, res) => {
+  const Users = await User.find();
+      res.send(users);
+});
+
+app.listen(3000, () => {
+  console.log("Server is running at port 3000");
 });
 
 mongoose
   .connect(
-    "mongodb://localhost:27017/mongodb+srv://Shukrah:<SHadie>@cluster0.ck5nl.mongodb.net/User?retryWrites=true&w=majority&appName=Cluster0"
+    "mongodb+srv://Shukrah:SHadie@cluster0.ck5nl.mongodb.net/UserInfo?retryWrites=true&w=majority&appName=Cluster0"
   )
   .then(() => {
     console.log("Connected to the database!");
@@ -39,7 +46,3 @@ mongoose
     console.log("Cannot connect to the database!", err);
     process.exit();
   });
-
-app.listen(3000, () => {
-  console.log("Server is running at port 3000");
-});
--- models/User.model.js
+++ models/User.model.js
@@ -1,7 +1,5 @@
 const mongoose = require("mongoose");
-const mongoose = require("mongoose");
-
-const { Schema } = mongoose;
+const Schema = mongoose.Schema;
 
 const UserSchema = new Schema({
   name: { type: String, required: true },
